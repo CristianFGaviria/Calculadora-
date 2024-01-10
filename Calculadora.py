a91f6c48b6e7fb6a039b8193009c0cee3001b0e2
CRISS <cristian94071@outlook.com>
2024-01-10 17:05:50 -0500

    Calculadora sencilla

---
+++ Calculadora.py
@@ -0,0 +1,55 @@
+import sys
+
+def suma ():
+    numero1 = int(input("Ingrese el primer numero: "))
+    numero2 = int(input("Ingrese el segundo numero: "))
+    resultado = numero1 + numero2
+    print(f"El resultado de la operacion es = {resultado}")
+def resta ():
+    numero1 = int(input("Ingrese el primer numero: "))
+    numero2 = int(input("Ingrese el segundo numero: "))
+    resultado = numero1 - numero2
+    print(f"El resultado de la operacion es = {resultado}")
+def multiplica ():
+    numero1 = int(input("Ingrese el primer numero: "))
+    numero2 = int(input("Ingrese el segundo numero: "))
+    resultado = numero1 * numero2
+    print(f"El resultado de la operacion es = {resultado}")
+def divide ():
+    numero1 = int(input("Ingrese el primer numero: "))
+    numero2 = int(input("Ingrese el segundo numero: "))
+    resultado = numero1 * numero2
+    print(f"El resultado de la operacion es = {resultado}")
+
+def salir ():
+    print("Hasta la proxima!!! ")
+    sys.exit()
+
+print("Bienvenido a la calculadora en Python")
+while True:
+    menu = """
+    Cual de las siguientes opciones desea ejecutar
+    [1] Sumar
+    [2] Restar
+    [3] Multiplicar
+    [4] Dividir
+    [5] Salir
+    """
+
+    print(menu)
+
+    opcion = int(input("Digite La opcion deseada: "))
+    if opcion == 1:
+        suma()
+    elif opcion == 2:
+        resta()
+    elif opcion == 3:
+        multiplica()
+    elif opcion == 4:
+        divide ()
+    elif opcion == 5:
+        salir()
+    else:
+        print("Usted selecciono una opcion no valida. Intente nuevamente")
+
+
